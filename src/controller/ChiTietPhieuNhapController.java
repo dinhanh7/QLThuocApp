7e8b2d91a29ba5e16426744b500fc7d062cd8ee1
Nguyen Dinh Anh <158207678+dinhanh7@users.noreply.github.com>
2025-06-15 00:01:26 +0700

    Update ChiTietPhieuNhapController.java

--- src/controller/ChiTietPhieuNhapController.java
+++ src/controller/ChiTietPhieuNhapController.java
@@ -9,6 +9,8 @@ public class ChiTietPhieuNhapController {
     public boolean addChiTietPhieuNhap(ChiTietPhieuNhap ct) {
         return chiTietPhieuNhapDAO.insert(ct);
     }
-
+    public void deleteByPhieuNhapAndThuoc(String idPN, String idThuoc) {
+        chiTietPhieuNhapDAO.deleteByPhieuNhapAndThuoc(idPN, idThuoc);
+    }
     // Bạn có thể thêm các phương thức khác nếu cần
 }
