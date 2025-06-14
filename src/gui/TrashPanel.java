0b95771aeaf4ae6c25d480f526e88ddde52e218a
lmhtung <142739482+lmhtung@users.noreply.github.com>
2025-06-14 15:39:16 +0700

    them trashpanel

--- src/gui/TrashPanel.java
+++ src/gui/TrashPanel.java
@@ -1,1 +1,23 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TrashPanel extends JPanel {
+
+    private JTabbedPane trashTabs;
+
+    public TrashPanel() {
+        setLayout(new BorderLayout());
+        trashTabs = new JTabbedPane();
+
+//        // Thêm các panel con cho từng bảng đã xóa mềm
+        trashTabs.addTab("Phản hồi", new TrashPhanHoiPanel());
+//        trashTabs.addTab("Khách hàng", new TrashKhachHangPanel());
+//        trashTabs.addTab("Nhà cung cấp", new TrashNhaCungCapPanel());
+//        trashTabs.addTab("Hóa đơn", new TrashHoaDonPanel());
+//        trashTabs.addTab("Hợp đồng", new TrashHopDongPanel());
+        add(trashTabs, BorderLayout.CENTER);
+    }
+}
 
