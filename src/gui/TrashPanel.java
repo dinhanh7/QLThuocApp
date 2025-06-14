2ad2dd0a0f60f6b28c2fbd9bd1a0997c6a779133
lmhtung <142739482+lmhtung@users.noreply.github.com>
2025-06-14 23:54:10 +0700

    Update TrashPanel.java

--- src/gui/TrashPanel.java
+++ src/gui/TrashPanel.java
@@ -15,17 +15,28 @@ public class TrashPanel extends JPanel {
         TrashHopDongPanel hopDongPanel = new TrashHopDongPanel(trashTabs, 1);
         TrashHoaDonPanel hoaDonPanel = new TrashHoaDonPanel(trashTabs, 2);
         TrashNhanVienPanel nhanVienPanel = new TrashNhanVienPanel(trashTabs, 3);
+        TrashThuocPanel ThuocPanel = new TrashThuocPanel(trashTabs, 4);
+        TrashNhaCungCapPanel NCCPanel = new TrashNhaCungCapPanel(trashTabs, 5);
+        TrashPhieuNhapPanel phieuNhapPanel = new TrashPhieuNhapPanel(trashTabs, 6);
+        
         
         trashTabs.addTab("Phản hồi", phanHoiPanel);
-	trashTabs.addTab("Hợp đồng", hopDongPanel);
-	trashTabs.addTab("Hóa đơn", hoaDonPanel);
+        trashTabs.addTab("Hợp đồng", hopDongPanel);
+        trashTabs.addTab("Hóa đơn", hoaDonPanel);
         trashTabs.addTab("Nhân viên", nhanVienPanel);
-		
-	phanHoiPanel.loadData();  // gọi loadData sau khi đã addTab
+        trashTabs.addTab("Thuốc", ThuocPanel);
+        trashTabs.addTab("Nhà cung cấp", NCCPanel);
+        trashTabs.addTab("Phiếu nhập", phieuNhapPanel);
+
+        
+        phanHoiPanel.loadData();  // gọi loadData sau khi đã addTab
         hopDongPanel.loadData();
         hoaDonPanel.loadData();
-	nhanVienPanel.loadData();
-
+        nhanVienPanel.loadData();
+        ThuocPanel.loadData();
+        NCCPanel.loadData();
+        phieuNhapPanel.loadData();
+        
         add(trashTabs, BorderLayout.CENTER);
     }
 
