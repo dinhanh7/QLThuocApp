077dd6e2b79960a34c566df6c9eaf227076314d4
lmhtung <142739482+lmhtung@users.noreply.github.com>
2025-06-14 23:02:41 +0700

    Update TrashPanel.java

--- src/gui/TrashPanel.java
+++ src/gui/TrashPanel.java
@@ -14,17 +14,18 @@ public class TrashPanel extends JPanel {
         TrashPhanHoiPanel phanHoiPanel = new TrashPhanHoiPanel(trashTabs, 0);
         TrashHopDongPanel hopDongPanel = new TrashHopDongPanel(trashTabs, 1);
         TrashHoaDonPanel hoaDonPanel = new TrashHoaDonPanel(trashTabs, 2);
-        
+        TrashNhanVienPanel nhanVienPanel = new TrashNhanVienPanel(trashTabs, 4);
         
         trashTabs.addTab("Phản hồi", phanHoiPanel);
-		trashTabs.addTab("Hợp đồng", hopDongPanel);
-		trashTabs.addTab("Hóa đơn", hoaDonPanel);
-        
+	trashTabs.addTab("Hợp đồng", hopDongPanel);
+	trashTabs.addTab("Hóa đơn", hoaDonPanel);
+        trashTabs.addTab("Nhân viên", nhanVienPanel);
 		
-		
-		phanHoiPanel.loadData();  // gọi loadData sau khi đã addTab
+	phanHoiPanel.loadData();  // gọi loadData sau khi đã addTab
         hopDongPanel.loadData();
         hoaDonPanel.loadData();
+	nhanVienPanel.loadData();
+
         add(trashTabs, BorderLayout.CENTER);
     }
 
